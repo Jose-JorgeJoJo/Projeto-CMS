b9849967b84f254256d76294623c99887be003d6
Cauã Barros <cauabarros07@gmail.com>
2026-03-19 16:11:20 -0300

    css e html do cms

--- Projeto Seleção/cms/classes/Dashboard.php
+++ Projeto Seleção/cms/classes/Dashboard.php
@@ -2,24 +2,12 @@
 
 class Dashboard {
     public function show() {
-       
         if (!isset($_SESSION['usuario'])) {
             header("Location: index.php?class=Login");
             exit;
         }
 
-        $usuario = $_SESSION['usuario'];
-
-        echo "<h1>Dashboard</h1>";
-        echo "<p>Bem-vindo, " . $usuario . "!</p>";
-
-        
-        echo '<a href="index.php?class=UsuarioList"><button>Usuários</button></a><br><br>';
-        echo '<a href="index.php?class=PreferenciaForm"><button>Preferências</button></a><br><br>';
-        echo '<a href="index.php?class=CaracteristicaList"><button>Características</button></a><br><br>';
-        echo '<a href="index.php?class=TestemunhoList"><button>Testemunhos</button></a><br><br>';
-        echo '<a href="index.php?class=MensagemList"><button>Mensagens</button></a><br><br>';
-
-        echo '<br><a href="index.php?class=Login&method=logout">Sair</a>';
+        $html = file_get_contents('cms/html/dashboard.html');
+        echo str_replace('{usuario}', $_SESSION['usuario'], $html);
     }
 }
